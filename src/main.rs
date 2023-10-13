4172528a84682f7cdcf97c4ee601db59e58b7755
dave horner <daveydave1234@gmail.com>
2023-10-13 19:46:17 -0400

    cargo fix/fmt

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,23 @@
 use anyhow::{Context, Result};
 use clap::Parser;
+use hyper::service::{make_service_fn, service_fn};
+use hyper::{Body, Request, Response, Server};
 use image::io::Reader as ImageReader;
-use image::{imageops, GenericImage, ImageBuffer, RgbaImage, Rgba};
+use image::{imageops, GenericImage, ImageBuffer, Rgba, RgbaImage};
 use image::{DynamicImage, ImageFormat};
 use infer::image::is_jpeg;
+use multer::Multipart;
 use ndarray::{Array, CowArray};
+use once_cell::sync::OnceCell;
 use ort::{Environment, ExecutionProvider, GraphOptimizationLevel, SessionBuilder, Value};
+use std::convert::Infallible;
 use std::fs;
 use std::io::{self, Cursor, Read, Write};
 use std::path::{Path, PathBuf};
 use std::time::Instant;
-use std::convert::Infallible;
-use hyper::{Body, Request, Response, Server};
-use hyper::service::{make_service_fn, service_fn};
-use once_cell::sync::OnceCell;
-use multer::Multipart;
 use tokio::io::AsyncWriteExt;
 
-static SESSION : OnceCell<ort::Session> = OnceCell::new();
+static SESSION: OnceCell<ort::Session> = OnceCell::new();
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -44,7 +44,6 @@ struct App {
     #[clap(short = 'H', long)]
     http: bool,
 }
-
 
 //#[tokio::main(flavor = "current_thread")]
 #[tokio::main(worker_threads = 10)]
@@ -233,7 +232,7 @@ fn process_image(
 
 fn onnx_session() -> Result<ort::Session, anyhow::Error> {
     let onnx_model_file = "assets/medium.onnx";
-        let environment = Environment::default().into_arc();
+    let environment = Environment::default().into_arc();
     let session = SessionBuilder::new(&environment)?
         .with_optimization_level(GraphOptimizationLevel::Level1)? // Configure model optimization level
         .with_intra_threads(1)? // Configure the number of threads used for inference
@@ -267,7 +266,7 @@ fn find_alpha_bounds(image: &RgbaImage) -> Option<(u32, u32, u32, u32)> {
     if min_x <= max_x && min_y <= max_y {
         Some((min_x, min_y, max_x, max_y))
     } else {
-        println!("found NONE {:?}",(min_x, min_y, max_x, max_y));
+        println!("found NONE {:?}", (min_x, min_y, max_x, max_y));
         None
     }
 }
@@ -280,7 +279,7 @@ fn process_dynamic_image(
         .dimensions()
         .map(|dim| dim.unwrap())
         .collect::<Vec<usize>>();
-        let input_img = dynamic_img.into_rgba8();
+    let input_img = dynamic_img.into_rgba8();
     let scaling_factor = f32::min(
         1., // Avoid upscaling
         f32::min(
@@ -294,7 +293,7 @@ fn process_dynamic_image(
         input_shape[2] as u32,
         imageops::FilterType::Triangle,
     );
-        let input_tensor = CowArray::from(
+    let input_tensor = CowArray::from(
         Array::from_shape_fn(input_shape, |indices| {
             let mean = 128.;
             let std = 256.;
@@ -303,31 +302,27 @@ fn process_dynamic_image(
         })
         .into_dyn(),
     );
-        let inputs = vec![Value::from_array(session.allocator(), &input_tensor)?];
+    let inputs = vec![Value::from_array(session.allocator(), &input_tensor)?];
     let outputs = session.run(inputs)?;
     let output_tensor = outputs[0].try_extract::<f32>()?;
     for (indices, alpha) in output_tensor.view().indexed_iter() {
         resized_img[(indices[3] as u32, indices[2] as u32)][3] = (alpha * 255.) as u8;
     }
-        let output_img = imageops::resize(
+    let output_img = imageops::resize(
         &resized_img,
         (input_img.width() as f32 * scaling_factor) as u32,
         (input_img.height() as f32 * scaling_factor) as u32,
         imageops::FilterType::Triangle,
     );
-        Ok(DynamicImage::ImageRgba8(output_img))
+    Ok(DynamicImage::ImageRgba8(output_img))
 }
 
 async fn start_http_server() -> Result<(), anyhow::Error> {
-     let session = onnx_session().unwrap();
-     SESSION.set(session).ok();
+    let session = onnx_session().unwrap();
+    SESSION.set(session).ok();
     // Define a closure to handle incoming HTTP requests
-    let make_svc = make_service_fn(|_conn| {
-     async {
-        Ok::<_, Infallible>(service_fn(move |req| {
-            handle_post(req)
-        }))
-    }
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(move |req| handle_post(req)))
     });
 
     let addr = ([0, 0, 0, 0], 9876).into();
@@ -339,93 +334,118 @@ async fn start_http_server() -> Result<(), anyhow::Error> {
 
 async fn handle_post(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
     let path = req.uri().clone().path().to_owned();
-    let session=SESSION.get().unwrap();
+    let session = SESSION.get().unwrap();
     // Check if the request is a multipart POST
     if let Some(content_type) = req.headers().get(hyper::header::CONTENT_TYPE) {
         if let Ok(content_type_str) = content_type.to_str() {
             if content_type_str.starts_with("multipart/form-data") {
-    // Parse the boundary from the content_type, exit early if we don't have it.
-    let boundary = match content_type_str.split(';').find(|s| s.trim().starts_with("boundary=")) {
-        Some(boundary) => boundary.trim().trim_start_matches("boundary=").to_string(),
-        None => {
-            eprintln!("Error: Boundary not found in CONTENT_TYPE.");
-            "".to_string()
-        }
-    };
-
-    let mut multipart = Multipart::new(req.into_body(), &boundary);
-    while let Some(mut field) = multipart.next_field().await.unwrap() {
-        let has_filename = field.file_name().map(|s| s.to_string());
-        if let Some(file_name) = has_filename {
-            let buffer = field.bytes().await.unwrap();
-    let img: Option<DynamicImage> = 
-        if is_jpeg(&buffer) {
-            Some(
-                ImageReader::with_format(io::Cursor::new(buffer), ImageFormat::Jpeg)
-                    .decode()
-                    .context("Failed to decode image from stdin").unwrap()
-            )
-        } else {
-            Some(
-                ImageReader::with_format(io::Cursor::new(buffer), ImageFormat::Png)
-                    .decode()
-                    .context("Failed to decode image from stdin").unwrap()
-            )
-        };
-
-    if img.is_some() {
-        let w=&img.clone().unwrap().clone().width();
-        println!("f: {}", file_name);
-        let processed_dynamic_img = process_dynamic_image(session, img.unwrap()).unwrap();
-
-            let mut crop_box: String = String::new();
-            let mut buffer = Cursor::new(Vec::new());
-            if path == "/crop" {
-                let mut output_img = processed_dynamic_img.to_rgba8();
-                let alpha_bounds = find_alpha_bounds(&output_img);
-                if let Some((min_x, min_y, max_x, max_y)) = alpha_bounds {
-                    let w=max_x-min_x;
-                    let h = max_y-min_y;
-                    if w>0 && h>0 {
-                        crop_box=format!("{},{},{},{}", min_x, min_y, max_x - min_x, max_y - min_y);
-                    let cropped_img =
-                        imageops::crop(&mut output_img, min_x, min_y, max_x - min_x, max_y - min_y)
-                            .to_image();
-                        cropped_img.write_to(&mut buffer, ImageFormat::Png).unwrap();
-                    } else {
-                        processed_dynamic_img.write_to(&mut buffer, ImageFormat::Png).unwrap();
+                // Parse the boundary from the content_type, exit early if we don't have it.
+                let boundary = match content_type_str
+                    .split(';')
+                    .find(|s| s.trim().starts_with("boundary="))
+                {
+                    Some(boundary) => boundary.trim().trim_start_matches("boundary=").to_string(),
+                    None => {
+                        eprintln!("Error: Boundary not found in CONTENT_TYPE.");
+                        "".to_string()
                     }
-                } else {
-                    // Set the pixel at (0, 0) to be fully transparent.
-                    let mut img: RgbaImage = ImageBuffer::new(1, 1);
-                    img.put_pixel(0, 0, Rgba([0, 0, 0, 0]));
-                    img.write_to(&mut buffer, ImageFormat::Png).unwrap();
+                };
+
+                let mut multipart = Multipart::new(req.into_body(), &boundary);
+                while let Some(mut field) = multipart.next_field().await.unwrap() {
+                    let has_filename = field.file_name().map(|s| s.to_string());
+                    if let Some(file_name) = has_filename {
+                        let buffer = field.bytes().await.unwrap();
+                        let img: Option<DynamicImage> = if is_jpeg(&buffer) {
+                            Some(
+                                ImageReader::with_format(
+                                    io::Cursor::new(buffer),
+                                    ImageFormat::Jpeg,
+                                )
+                                .decode()
+                                .context("Failed to decode image from stdin")
+                                .unwrap(),
+                            )
+                        } else {
+                            Some(
+                                ImageReader::with_format(io::Cursor::new(buffer), ImageFormat::Png)
+                                    .decode()
+                                    .context("Failed to decode image from stdin")
+                                    .unwrap(),
+                            )
+                        };
+
+                        if img.is_some() {
+                            let w = &img.clone().unwrap().clone().width();
+                            println!("f: {}", file_name);
+                            let processed_dynamic_img =
+                                process_dynamic_image(session, img.unwrap()).unwrap();
+
+                            let mut crop_box: String = String::new();
+                            let mut buffer = Cursor::new(Vec::new());
+                            if path == "/crop" {
+                                let mut output_img = processed_dynamic_img.to_rgba8();
+                                let alpha_bounds = find_alpha_bounds(&output_img);
+                                if let Some((min_x, min_y, max_x, max_y)) = alpha_bounds {
+                                    let w = max_x - min_x;
+                                    let h = max_y - min_y;
+                                    if w > 0 && h > 0 {
+                                        crop_box = format!(
+                                            "{},{},{},{}",
+                                            min_x,
+                                            min_y,
+                                            max_x - min_x,
+                                            max_y - min_y
+                                        );
+                                        let cropped_img = imageops::crop(
+                                            &mut output_img,
+                                            min_x,
+                                            min_y,
+                                            max_x - min_x,
+                                            max_y - min_y,
+                                        )
+                                        .to_image();
+                                        cropped_img
+                                            .write_to(&mut buffer, ImageFormat::Png)
+                                            .unwrap();
+                                    } else {
+                                        processed_dynamic_img
+                                            .write_to(&mut buffer, ImageFormat::Png)
+                                            .unwrap();
+                                    }
+                                } else {
+                                    // Set the pixel at (0, 0) to be fully transparent.
+                                    let mut img: RgbaImage = ImageBuffer::new(1, 1);
+                                    img.put_pixel(0, 0, Rgba([0, 0, 0, 0]));
+                                    img.write_to(&mut buffer, ImageFormat::Png).unwrap();
+                                }
+                            } else {
+                                processed_dynamic_img
+                                    .write_to(&mut buffer, ImageFormat::Png)
+                                    .unwrap();
+                            }
+                            let buffer_content = buffer.into_inner();
+                            let mut response = Response::new(Body::from(buffer_content));
+                            if !crop_box.is_empty() {
+                                response.headers_mut().insert(
+                                    "Access-Control-Expose-Headers",
+                                    hyper::header::HeaderValue::from_static("*"),
+                                );
+                                response.headers_mut().insert(
+                                    "X-cropbox",
+                                    hyper::header::HeaderValue::from_str(&crop_box).unwrap(),
+                                );
+                            }
+                            response.headers_mut().insert(
+                                hyper::header::ACCESS_CONTROL_ALLOW_ORIGIN,
+                                hyper::header::HeaderValue::from_static("*"),
+                            );
+                            return Ok(response);
+                        } else {
+                            println!("no image");
+                        }
+                    }
                 }
-            } else {
-                processed_dynamic_img.write_to(&mut buffer, ImageFormat::Png).unwrap();
-            }
-            let buffer_content = buffer.into_inner();
-            let mut response = Response::new(Body::from(buffer_content));
-            if !crop_box.is_empty() {
-                response.headers_mut().insert(
-                "Access-Control-Expose-Headers",
-                hyper::header::HeaderValue::from_static("*"),
-                               );
-               response.headers_mut().insert(
-                "X-cropbox",
-                hyper::header::HeaderValue::from_str(&crop_box).unwrap(),
-            );
-            }
-            response.headers_mut().insert(
-                hyper::header::ACCESS_CONTROL_ALLOW_ORIGIN,
-                hyper::header::HeaderValue::from_static("*"),
-            );
-            return Ok(response)
-    } else {
-        println!("no image");
-    }
-        }
-    }
                 let response = Response::new(Body::from("File uploaded successfully"));
                 return Ok(response);
             }
