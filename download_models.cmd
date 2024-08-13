9488f5b0a0a0d800c57f1af83a780dabbfb3780a
Khoa Duong <dnakhoa@icloud.com>
2024-08-14 06:40:49 +0700

    chore(download_models): update model links (fix #6)

--- download_models.cmd
+++ download_models.cmd
@@ -1,4 +1,4 @@
 mkdir -p "assets"
-curl -L "https://github.com/imgly/background-removal-js/raw/main/bundle/models/small" -o "assets/small.onnx"
-curl -L "https://github.com/imgly/background-removal-js/raw/main/bundle/models/medium" -o "assets/medium.onnx"
-curl -L "https://github.com/imgly/background-removal-js/raw/main/bundle/models/large" -o "assets/large.onnx"
+curl -L "https://github.com/imgly/background-removal-js/raw/4306d99530d3ae9ec11a892a23802be28f367518/bundle/models/small" -o "assets/small.onnx"
+curl -L "https://github.com/imgly/background-removal-js/raw/4306d99530d3ae9ec11a892a23802be28f367518/bundle/models/medium" -o "assets/medium.onnx"
+curl -L "https://github.com/imgly/background-removal-js/raw/4306d99530d3ae9ec11a892a23802be28f367518/bundle/models/large" -o "assets/large.onnx"
--- download_models.sh
+++ download_models.sh
@@ -4,10 +4,10 @@
 mkdir -p 'assets'
 
 # Download small model
-curl -L 'https://github.com/imgly/background-removal-js/raw/main/bundle/models/small' -o 'assets/small.onnx'
+curl -L 'https://github.com/imgly/background-removal-js/raw/4306d99530d3ae9ec11a892a23802be28f367518/bundle/models/small' -o 'assets/small.onnx'
 
 # Download medium model
-curl -L 'https://github.com/imgly/background-removal-js/raw/main/bundle/models/medium' -o 'assets/medium.onnx'
+curl -L 'https://github.com/imgly/background-removal-js/raw/4306d99530d3ae9ec11a892a23802be28f367518/bundle/models/medium' -o 'assets/medium.onnx'
 
 # Download large model
-curl -L 'https://github.com/imgly/background-removal-js/raw/main/bundle/models/large' -o 'assets/large.onnx'
+curl -L 'https://github.com/imgly/background-removal-js/raw/4306d99530d3ae9ec11a892a23802be28f367518/bundle/models/large' -o 'assets/large.onnx'
